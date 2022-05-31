fac88821b986ffc2fe6c47964260e45edebbf8db
fufako <patryko.mikiciuk@gmail.com>
2022-05-31 17:16:37 +0200

    Add if statments to check if input is empty. checkIfValid function checks mail and phone number

---
+++ script.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll("input")
+for (input of inputs) {
+  input.addEventListener("focusout", checkIfValid)
+}
+
+function checkIfValid(e) {
+  if (
+    (e.target.value === "" || e.target.value == null) &&
+    e.target.id !== "confirm_password"
+  ) {
+    e.target.nextElementSibling.textContent =
+      e.target.previousElementSibling.textContent + " can not be empty"
+  }
+
+  if (e.target.id === "mail") {
+    if (e.target.value.match(/^\S+@\S+\.\S+$/)) {
+      e.target.nextElementSibling.textContent =
+        e.target.previousElementSibling.textContent + " looks good 😁"
+    } else {
+      e.target.nextElementSibling.textContent =
+        e.target.previousElementSibling.textContent + " format is invalid"
+    }
+  } else if (e.target.id === "phone_number") {
+    if (e.target.value.replaceAll(" ", "").match(/\d{9}/)) {
+      e.target.nextElementSibling.textContent =
+        e.target.previousElementSibling.textContent + " looks good 😁"
+    } else {
+      e.target.nextElementSibling.textContent =
+        e.target.previousElementSibling.textContent +
+        " format is invalid example: 123 456 789"
+    }
+  }
+}
